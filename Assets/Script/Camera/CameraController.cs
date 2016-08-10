d6b193ca927763fb3ed922310836027ad2fb1947
Eclmist <Eclmist@yahoo.com>
2016-08-11 00:21:53 +0800

    Initial Commit

---
+++ Assets/Script/Camera/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform player;
+    public float rotateSpeed = 5;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate()
+    {
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+
+        transform.eulerAngles += new Vector3(-vertical, horizontal, 0);
+
+        float fov = Camera.main.fieldOfView;
+        float newFov = Input.GetAxis("Mouse ScrollWheel") * 30;
+        newFov = Mathf.Clamp(fov, 50, 80);
+
+        fov = Mathf.Lerp(fov, newFov, Time.deltaTime);
+
+        Camera.main.fieldOfView = fov;
+    }
+
+}
---
+++ Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+[RequireComponent (typeof(Animator))]
+[RequireComponent (typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+
+    private Animator animator;
+    private Rigidbody rb;
+    private Vector3 movementDirection;
+
+    void Start () {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        movementDirection = Vector3.zero;
+	}
+	
+	void Update () {
+	}
+
+    void Move()
+    {      
+         //movementDirection = Input.GetAxis(Horizontal)
+    }
+}
